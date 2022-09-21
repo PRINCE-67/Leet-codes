565e11aaa0eed184e8d01bcdcd2285b4495a09ee
Prince Verma <80540136+PRINCE-67@users.noreply.github.com>
2022-09-21 12:45:05 +0530

    Time: 2 ms (59.10%), Space: 7.8 MB (49.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     
-    int ways(vector<int>& a,int n,vector<int>& dp){
-        if(n==0) return a[0];
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        int np=ways(a,n-1,dp);
-        int p=INT_MIN;
-        p=a[n];
-        if(n>1){
-            p+=ways(a,n-2,dp);
-        }
-        
-        return dp[n]=max(p,np);
-    }
-    
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return ways(nums,nums.size()-1,dp);
+          int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        
+        for(int i=1;i<n;i++){
+            int pick=nums[i]+prev2;
+            int not_pick=prev;
+            
+            int curi=max(pick,not_pick);
+            
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
     }
 };
